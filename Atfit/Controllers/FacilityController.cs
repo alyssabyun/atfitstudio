f6e41118aa08d07eede914f797f039dbb3c8fe72
Alyssa Byun <alyssa.byun@hotmail.com>
2014-07-01 13:52:31 +1200

    30mins add hr, active menu, created all controllers

---
+++ Atfit/Controllers/FacilityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atfit.Controllers
+{
+    public class FacilityController : Controller
+    {
+        //
+        // GET: /Facility/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Atfit/Controllers/ProgrammeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atfit.Controllers
+{
+    public class ProgrammeController : Controller
+    {
+        //
+        // GET: /Programme/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
