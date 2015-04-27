a9a9bf4512ec2cd304fd46c5c73f904a4fbc8e9b
Alyssa Byun <alyssa.byun@hotmail.com>
2015-04-27 22:15:54 +1200

    edited

---
+++ Atfit/Controllers/ReferController.cs
@@ -0,0 +1,43 @@
+﻿using Atfit.Models;
+using Atfit.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atfit.Controllers
+{
+    public class ReferController : Controller
+    {
+        //
+        // GET: /Refer/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new ReferFriendModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ReferFriendModel m)
+        {
+            if(TryValidateModel(m) && !m.IsSpam)
+            {
+                var email = new EmailModel();
+
+                email.FromEmail = Config.Current.EmailNoReply.Address;
+                email.FromName = m.Name;
+                email.Subject = "Refer a friend";
+                email.Message = "Friend's name: " + m.FriendName + ",<br /> Friend's phone number: " + m.Phone + "<br /> My name: " + m.Name ;
+                email.ReplyToAddress = email.FromEmail;
+
+                var emailService = new EmailService();
+                emailService.SendEmail(email);
+                return View("Submitted");
+            }
+            return View(m);
+        }
+    }
+}
---
+++ Atfit/Models/ReferFriendModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atfit.Models
+{
+    public class ReferFriendModel
+    {
+        [Required]
+        public string FriendName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string AntiSpam { get; set; }
+        public bool IsSpam
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(AntiSpam);
+            }
+        }
+    }
+}
